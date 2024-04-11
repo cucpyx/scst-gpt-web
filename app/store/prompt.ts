6b213a3a853c794f7807aceb88e066b035470b4c
SCST学生会 <67895835+cucpyx@users.noreply.github.com>
2024-04-11 15:31:59 +0800

    Update prompt.ts

--- app/store/prompt.ts
+++ app/store/prompt.ts
@@ -154,7 +154,8 @@ export const usePromptStore = createPersistStore(
       fetch(PROMPT_URL)
         .then((res) => res.json())
         .then((res) => {
-          let fetchPrompts = [res.en, res.cn];
+          // let fetchPrompts = [res.en, res.cn];
+          let fetchPrompts = [res.cn];
           if (getLang() === "cn") {
             fetchPrompts = fetchPrompts.reverse();
           }
@@ -175,7 +176,8 @@ export const usePromptStore = createPersistStore(
           const allPromptsForSearch = builtinPrompts
             .reduce((pre, cur) => pre.concat(cur), [])
             .filter((v) => !!v.title && !!v.content);
-          SearchService.count.builtin = res.en.length + res.cn.length;
+          // SearchService.count.builtin = res.en.length + res.cn.length;
+          SearchService.count.builtin = res.cn.length;
           SearchService.init(allPromptsForSearch, userPrompts);
         });
     },
